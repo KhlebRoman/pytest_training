3ea3613f9f4a55c2be9ec9def6609381ee88bb75
RomanKhleb <rimusdix@gmail.com>
2021-10-19 15:20:58 +0300

    Create new class SessionHelper for managing session

--- test/TestAuth.py
+++ test/TestAuth.py
@@ -12,6 +12,6 @@ def app(request):
 
 
 def test_auth(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(group_name="группа1", group_footer="описание", group_header="сыфсфы"))
-    app.logout()
+    app.session.logout()
