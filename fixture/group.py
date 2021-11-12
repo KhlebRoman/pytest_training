0fdfd5ce97d16c8d4b2c44298853428642b8144f
RomanKhleb <rimusdix@gmail.com>
2021-11-12 14:29:29 +0300

    create new asserts

--- fixture/group.py
+++ fixture/group.py
@@ -1,10 +1,13 @@
+from model.group import Group
+
 class GroupHelper:
     def __init__(self, app):
         self.app = app
 
     def open_groups_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("name")) > 0):
+            wd.find_element_by_link_text("groups").click()
 
     def create(self, group):
         wd = self.app.wd
@@ -54,3 +57,13 @@ class GroupHelper:
         wd = self.app.wd
         self.open_groups_page()
         return len(wd.find_elements_by_name("selected[]"))
+
+    def get_group_list(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        groups = []
+        for element in wd.find_elements_by_css_selector("span.group"):
+            text = element.text
+            id = element.find_element_by_name("selected[]").get_attribute("value")
+            groups.append(Group(group_name=text, id=id))
+        return groups
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,9 +2,22 @@
 from model.group import Group
 
 
+
 def test_add_group(app):
-    app.group.create(Group(group_name="группа1", group_footer="описание", group_header="сыфсфы"))
+    old_groops = app.group.get_group_list()
+    group = Group(group_name="группа", group_footer="описание", group_header="сыфсфы")
+    app.group.create(group)
+    new_groops = app.group.get_group_list()
+    assert len(old_groops) + 1 == len(new_groops)
+    old_groops.append(group)
+    assert sorted(old_groops, key=Group.id_or_max) == sorted(new_groops, key=Group.id_or_max)
 
 
 def test_add_empty_group(app):
-    app.group.create(Group(group_name="", group_footer="", group_header=""))
+    old_groops = app.group.get_group_list()
+    group = Group(group_name="", group_footer="", group_header="")
+    app.group.create(group)
+    new_groops = app.group.get_group_list()
+    assert len(old_groops) + 1 == len(new_groops)
+    old_groops.append(group)
+    assert sorted(old_groops, key=Group.id_or_max) == sorted(new_groops, key=Group.id_or_max)
